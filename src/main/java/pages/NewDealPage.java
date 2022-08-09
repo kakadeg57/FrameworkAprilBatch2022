3373e7c758879f29d8e2bb3ba72b0cc4a17c423e
Shraddha Mahajan <shraddhamahajan444@gmail.com>
2022-08-10 00:30:15 +0530

    WebElements and PageFactory added

--- src/main/java/pages/NewDealPage.java
+++ src/main/java/pages/NewDealPage.java
@@ -1,5 +1,22 @@
 package pages;
 
-public class NewDealPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.TestBase;
+
+public class NewDealPage extends TestBase {
+	//Elements initialization
+	
+	public NewDealPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Elements repo
+	@FindBy(xpath="")
+	WebElement element;
+	
+	//action
 
 }
