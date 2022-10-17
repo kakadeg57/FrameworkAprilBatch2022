ba0dde082ba3683c7182c0f0e5d5e287e25a6db4
yogita <yogitavc43@gmail.com>
2022-10-17 20:18:26 +0530

    webelement added

--- src/main/java/pages/NewDealPage.java
+++ src/main/java/pages/NewDealPage.java
@@ -1,5 +1,18 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class NewDealPage {
 
+	
+	
+	@FindBy (xpath="xyz")
+	WebElement element;
+	
+	
+	
+	
+	
+	
 }
